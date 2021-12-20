a0f4322ad2b299cf2d2f1073582edb5866b3772f
rBrenick <richard.brenick@embark-studios.com>
2021-12-20 13:14:56 +0100

    added houdini support to qt-tool-template

--- tool_name/tool_name_ui.py
+++ tool_name/tool_name_ui.py
@@ -8,14 +8,14 @@ standalone_app = None
 if not QtWidgets.QApplication.instance():
     standalone_app = QtWidgets.QApplication(sys.argv)
 
-        
+
 class ToolNameWindow(ui_utils.ToolWindow):
     def __init__(self):
         super(ToolNameWindow, self).__init__()
-        
+
         # add simple button example
         self.add_button("Example Button", self.example_action)
-    
+
     def example_action(self):
         print("Example button triggered")
 
@@ -23,8 +23,9 @@ class ToolNameWindow(ui_utils.ToolWindow):
 def main(refresh=False):
     win = ToolNameWindow()
     win.main(refresh=refresh)
-    
+
     if standalone_app:
+        ui_utils.standalone_app_window = win
         sys.exit(standalone_app.exec_())
 
 
