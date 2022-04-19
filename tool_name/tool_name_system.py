d3d857b471e1141e55aefba0f23d6a442a54fc87
rBrenick <richard.brenick@embark-studios.com>
2022-04-19 10:19:27 +0200

    added dcc core interface when missing implementation

--- tool_name/tool_name_system.py
+++ tool_name/tool_name_system.py
@@ -6,3 +6,6 @@ active_dcc_is_maya = "maya" in os.path.basename(sys.executable)
 if active_dcc_is_maya:
     from . import tool_name_dcc_maya as dcc_module
     dcc = dcc_module.ToolNameMaya()
+else:
+    from . import tool_name_dcc_core as dcc_module
+    dcc = dcc_module.ToolNameCoreInterface()
